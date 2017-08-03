16bd1f372c2d20f561ef34b12765d60c3a38b93e
logonrm <logonrm@fiap.com.br>
2017-08-03 11:53:22 -0300

    Exercicio começado

---
+++ Fiap.HelloWorld/Exercicio01/Model/Conta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Banco.Model
+{
+    abstract class Conta
+    {
+        public int Agencia { get; set; }
+
+        public DateTime dataAbertura { get; set; }
+
+        public int Numero { get; set; }
+
+        public decimal Saldo { get; set; }
+
+        public abstract void Depositar(decimal valor);
+
+        public abstract void Retirar(decimal valor)
+
+    }
+}
---
+++ Fiap.HelloWorld/Exercicio01/Model/ContaCorrente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Banco.Model
+{
+    class ContaCorrente : Conta
+    {
+        public TipoConta TipoConta { get; set; }
+
+        public override void Depositar(decimal valor)
+        {
+            Saldo += valor;
+        }
+
+        public override void Retirar(decimal valor)
+        {
+            Saldo -= valor;
+
+            if (TipoConta == TipoConta.Comum && Saldo < 0)
+            {
+                throw new System.ArgumentException("Seu saldo ficara negativo") ;
+            }
+            {
+
+            }
+        }
+    }
+}
