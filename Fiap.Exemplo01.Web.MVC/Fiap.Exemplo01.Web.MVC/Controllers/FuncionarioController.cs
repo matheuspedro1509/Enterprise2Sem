09615cf36d2d74a2afae6dd8b8e0451372812e1f
logonrm <logonrm@fiap.com.br>
2017-08-10 11:24:27 -0300

    MVC asp.net

---
+++ Fiap.Exemplo01.Web.MVC/Fiap.Exemplo01.Web.MVC/Controllers/FuncionarioController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fiap.Exemplo01.Web.MVC.Models;
+
+namespace Fiap.Exemplo01.Web.MVC.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        // GET: Funcionario
+        [HttpGet]
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Gravar(Funcionario func)
+        {
+            ViewBag.nome = func.Nome;
+            ViewBag.data = func.Data;
+            ViewBag.salario = func.Salario;
+            return View(func);
+        }
+    }
+}
---
+++ Fiap.Exemplo01.Web.MVC/Fiap.Exemplo01.Web.MVC/Models/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Exemplo01.Web.MVC.Models
+{
+    public class Funcionario
+    {
+        public String Nome { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public Decimal Salario { get; set; }
+
+    }
+}
