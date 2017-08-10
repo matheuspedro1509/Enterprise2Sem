3bf1a9c469f3ec28fb544a18ce3adb9d3fb5983f
logonrm <logonrm@fiap.com.br>
2017-08-10 11:44:52 -0300

    Inicio exercicio asp.net

---
+++ Fiap.Exemplo01.Web.MVC/Fiap.Exemplo01.Web.MVC/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo01.Web.MVC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Confirmacao()
+        {
+            return View();
+        }
+
+    }
+}
