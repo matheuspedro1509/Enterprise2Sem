bbe5ab245bcc64c92208b9fdd1c621ebdc545977
logonrm <logonrm@fiap.com.br>
2017-08-24 11:47:00 -0300

    Inicio de Entity Framework

---
+++ Fiap.Exemplo03.Web.MVC/Fiap.Exemplo03.Web.MVC/Controllers/ApostadorController.cs
@@ -0,0 +1,34 @@
+﻿using Fiap.Exemplo03.Web.MVC.Models;
+using Fiap.Exemplo03.Web.MVC.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo03.Web.MVC.Controllers
+{
+    public class ApostadorController : Controller
+    {
+
+        private static List<LoteriaContext> banco = new List<LoteriaContext>();
+
+        // GET: Apostador
+        [HttpGet]
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastro(Apostador apost)
+        {
+            //grava no banco
+            banco.Add(apost);
+            //Mensagem de sucesso
+            TempData["msg"] = "Cadastrado";
+            ViewBag.mensagem = "Cadastrou";
+            return RedirectToAction("Cadastro");
+        }
+    }
+}
---
+++ Fiap.Exemplo03.Web.MVC/Fiap.Exemplo03.Web.MVC/Models/Apostador.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Exemplo03.Web.MVC.Models
+{
+    public class Apostador
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        public String Email { get; set; }
+
+
+        public DateTime DataNascimento { get; set; }
+
+
+        [Required]
+        [Column("SaldoTotal")]
+        public decimal Saldo { get; set; }
+    }
+}
